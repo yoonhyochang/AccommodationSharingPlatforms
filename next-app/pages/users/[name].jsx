17d2d6f69017e0f30a96e5595152b6d855162e61
gptus <yhc9308@naver.com>
2023-09-23 23:42:00 +0900

    깃 허브 프로필 스타일링

--- next-app/pages/users/[name].jsx
+++ next-app/pages/users/[name].jsx
@@ -1,37 +1,76 @@
+import fetch from "isomorphic-unfetch";
 import css from "styled-jsx/css";
 
 const style = css`
-  h2 {
-    margin-left: 20px;
+  .profile-box {
+    width: 25%;
+    max-width: 272px;
+    margin-right: 26px;
   }
-  .user-bio {
-    margin-top: 12px;
-    font-style: italic;
+  .profile-image-wrapper {
+    width: 100%;
+    border: 1px solid #e1e4e8;
+  }
+  .profile-image-wrapper .profile-image {
+    display: block;
+    width: 100%;
+  }
+  .profile-user-login {
+    margin: 0;
+    font-size: 20px;
+  }
+  .profile-username {
+    margin: 0;
+    padding-top: 16px;
+    font-size: 26px;
+  }
+  .profile-user-login {
+    margin: 0;
+    font-size: 20px;
+  }
+  .profile-user-bio {
+    margin: 0;
+    padding-top: 16px;
+    font-size: 14px;
   }
 `;
 
-const Username = ({ user }) => {
+const name = ({ user }) => {
+  if (!user) {
+    return null;
+  }
   return (
     <>
-      {user ? (
+      <div className="profile-box">
         <div>
-          <h2>{user.name}</h2>
-          <p className="user-bio">{user.bio}</p>
+          <img
+            className="profile-image-wrapper"
+            src={user.avatar_url}
+            alt={`${user.name} 프로필 이미지`}
+          />
         </div>
-      ) : (
-        <div>유저 정보가 없습니다</div>
-      )}
+        <h2 className="profile-username">{user.name}</h2>
+        <p className="profile-user-login">{user.login}</p>
+        <p className="profile-user-bio">{user.bio}</p>
+      </div>
       <style jsx>{style}</style>
     </>
   );
 };
 
-function ParentComponent() {
-  return (
-    <div>
-      <Username user={{ name: "jerrynim", bio: "Basic in the end" }} />
-    </div>
-  );
-}
+export const getServerSideProps = async ({ query }) => {
+  const { name } = query;
+  try {
+    const userRes = await fetch(`https://api.github.com/users/${name}`);
+    if (userRes.status === 200) {
+      const user = await userRes.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e.message);
+    return { props: {} };
+  }
+};
 
-export default ParentComponent;
+export default name;
