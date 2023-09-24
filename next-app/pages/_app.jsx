f047a5da541f3c6b2977b87ff06d0533d6d2a1f4
gptus <yhc9308@naver.com>
2023-09-24 14:34:17 +0900

    공통 헤더 만들기

--- next-app/pages/_app.jsx
+++ next-app/pages/_app.jsx
@@ -1,13 +1,10 @@
+import Header from "../components/Header";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      {" "}
+      <Header />
       <Component {...pageProps} />
-      <style jsx global>{`
-        body {
-          margin: 0;
-        }
-      `}</style>
     </>
   );
 };
