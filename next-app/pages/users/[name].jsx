e21076ef9c4cbd598b8e2c280159b9758781d4cd
gptus <yhc9308@naver.com>
2023-09-24 11:39:32 +0900

    깃 허브 라지토리 리스트 스타일링 1

--- next-app/pages/users/[name].jsx
+++ next-app/pages/users/[name].jsx
@@ -15,15 +15,26 @@ const name = ({ user }) => {
 export const getServerSideProps = async ({ query }) => {
   const { name } = query;
   try {
+    let user;
+    let repos;
+
     const userRes = await fetch(`https://api.github.com/users/${name}`);
     if (userRes.status === 200) {
-      const user = await userRes.json();
+      user = await userRes.json();
       console.log("user :", user);
-      return { props: { user } };
     }
-    return { props: {} };
+    const repoRes = await fetch(
+      `https://api.github.com/users/${name}/repos?sort=updated&page=1&per_page=10`
+    );
+
+    if (repoRes.status === 200) {
+      repos = await repoRes.json();
+      // console.log("repos :", repos);
+    }
+    console.log("repos :", repos);
+    return { props: { user, repos } };
   } catch (e) {
-    console.log("e.message", e.message);
+    console.log("e :", e);
     return { props: {} };
   }
 };
