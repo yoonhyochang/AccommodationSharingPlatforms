748f8fa31c971d4fc6058821b0d3979e9bcfaa47
gptus <yhc9308@naver.com>
2023-09-24 20:44:11 +0900

    글로벌 스타일 설정하기
    yarn add styled-reset 버전 문제로 노드 18.18.0 변경 후 설치

--- next-todo/pages/index.tsx
+++ next-todo/pages/index.tsx
@@ -3,11 +3,22 @@ import { NextPage } from "next";
 import styled from "styled-components";
 
 const Container = styled.div`
-  font-style: italic;
+  padding: 20px;
 `;
 
 const index: NextPage = () => {
-  return <Container>hello Styled-components</Container>;
+  return (
+    <Container>
+      <h1>hello Styled-components</h1>
+      <h2>hello Styled-components</h2>
+      <p>hello Styled-components</p>
+      <ul>
+        <li>hello Styled-components</li>
+        <a>hello Styled-components</a>
+        <span>hello Styled-components</span>
+      </ul>
+    </Container>
+  );
 };
 
 export default index;
--- next-todo/styles/GlobalStyle.ts
+++ next-todo/styles/GlobalStyle.ts
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  * {
+    box-sizing: border-box;
+  }
   body {
    margin: 0;
   }
