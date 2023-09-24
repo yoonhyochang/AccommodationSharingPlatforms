4f1c8cfea2d8d05e8849fd616a3076fd1aa6c47f
gptus <yhc9308@naver.com>
2023-09-24 21:19:58 +0900

    투두리스트 헤더 만들기

--- next-todo/pages/_app.tsx
+++ next-todo/pages/_app.tsx
@@ -1,10 +1,12 @@
-import App, { AppContext, AppProps, AppOnitialProps } from "next/app";
+import { AppProps } from "next/app";
 import GlobalStyle from "../styles/GlobalStyle";
+import Header from "../components/Header";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
--- next-todo/pages/_document.tsx
+++ next-todo/pages/_document.tsx
@@ -1,18 +1,22 @@
-import Document from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />)
         });
-
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -27,4 +31,27 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
