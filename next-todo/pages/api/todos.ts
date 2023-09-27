e709d2edddf81243fa33b3c9f4d4da110be7101d
gptus <yhc9308@naver.com>
2023-09-27 21:43:09 +0900

    넥스트 api 사용해보기 수정

--- next-todo/pages/api/todos.ts
+++ next-todo/pages/api/todos.ts
@@ -1,5 +1,32 @@
+import fs from "fs";
+import { join } from "path";
 import { NextApiRequest, NextApiResponse } from "next";
+import { TodoType } from "../../types/todo";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const todos = await new Promise<TodoType[]>((resolve, reject) => {
+        fs.readFile(join(process.cwd(), "data/todos.json"), (err, data) => {
+          if (err) {
+            return reject(err.message);
+          }
+          const todosData = data.toString();
+          if (!todosData) {
+            //* todos.json값이 비어있다면
+            return resolve([]);
+          }
+          const todos = JSON.parse(data.toString());
+          return resolve(todos);
+        });
+      });
+      res.statusCode = 200;
+      return res.send(todos);
+    } catch (e) {
+      console.log(e);
+      res.statusCode = 500;
+      res.send(e);
+    }
+  }
   return res.send("hello Next!");
 };
