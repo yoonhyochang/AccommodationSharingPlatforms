507402d6464d7eb7899386dbd9676ac1adeffe0a
gptus <yhc9308@naver.com>
2023-09-27 23:50:50 +0900

    axios 설정하기

--- next-todo/lib/api/index.ts
+++ next-todo/lib/api/index.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
-
+//기존 에러문제로 이미 만들어 놓음
 const axios = Axios.create({
-  baseURL: "http://localhost:3000/api/todos"
+  baseURL: "http://localhost:3000"
 });
 
 export default axios;
--- next-todo/pages/index.tsx
+++ next-todo/pages/index.tsx
@@ -3,25 +3,20 @@ import { GetServerSideProps, NextPage } from "next";
 import Axios from "axios";
 import TodoList from "../components/TodoList";
 import todos from "../data/todos.json";
-import { TodoType } from "../types/todo";
+import { getTodosAPI } from "../lib/api/todo";
 
 const app: NextPage = () => {
   return <TodoList todos={todos} />;
 };
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
-    const { data } = await Axios.get<TodoType[]>(
-      "http://localhost:3000/api/todos"
-    );
+    const { data } = await getTodosAPI();
 
     console.log("data :", data);
 
-    // 배열 안에 data 만 필요해서
-    // const res = await Axios.get("http://localhost:3000/api/todos");
-    // console.log("res :", res);
     return { props: {} };
   } catch (e) {
-    console.log(e);
+    console.log("e :", e);
     return { props: {} };
   }
 };
